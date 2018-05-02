08e0c455cab433f4a9cfcec7c864dbcf39694388
Sean12mps <sean.michael.piettojo@gmail.com>
2018-05-02 19:22:45 +0700

    [Fixes #49] Add sub_item_slug meta field in product edit.

--- admin/partials/wc-muse-product-tab.php
+++ admin/partials/wc-muse-product-tab.php
@@ -2,6 +2,7 @@
 $post_id = $_GET['post']; 
 $item_slug = get_post_meta( $post_id, 'item_slug', true );
 $seat_slug = get_post_meta( $post_id, 'seat_slug', true );
+$sub_item_slug = get_post_meta( $post_id, 'sub_item_slug', true );
 $ticket_type = get_post_meta( $post_id, 'ticket_type', true );
 ?>
 <div id="general_product_muse" class="panel woocommerce_options_panel" style="display:block">
@@ -10,6 +11,13 @@ $ticket_type = get_post_meta( $post_id, 'ticket_type', true );
 		<p class="form-field item_slug_field ">
 			<label for="item_slug"><?php _e( 'Item Slug', 'wc-muse' ) ?></label>
 			<input type="text" name="item_slug" id="item_slug" value="<?php echo $item_slug; ?>">
+		</p>
+	</div>
+
+	<div class="options-group">
+		<p class="form-field sub_item_slug_field ">
+			<label for="sub_item_slug"><?php _e( 'Sub Item Slug', 'wc-muse' ) ?></label>
+			<input type="text" name="sub_item_slug" id="sub_item_slug" value="<?php echo $sub_item_slug; ?>">
 		</p>
 	</div>
 
